a06d6313af31e6a4da894588378060c0f568268d
louis.sussan@gmail.com <louis.sussan@gmail.com>
2017-07-29 10:41:14 -0700

    update bamazon customer code

---
+++ bamazon.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS bamazon_db;
+
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products (
+	`item_id` INTEGER(11) AUTO_INCREMENT NOT NULL,
+    `product_name` VARCHAR(100) NOT NULL,
+    `department_name` VARCHAR(100) NOT NULL,
+    `price` DECIMAL(10,2) NOT NULL,
+    `stock_quantity` INTEGER(10) NOT NULL,
+    PRIMARY KEY (item_id)
+    
+);
+
+select * from products;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+	  VALUES('turntable', 'electronics', 199.99, 10),
+			('Air Force Ones', 'shoes', 129.99, 25),
+			('iphone charger', 'electronics', 9.99, 100),
+			('Magic Johnson Autographed Basketball', 'memorabilia', 999.99, 2),
+			('Arcade Fire Funeral LP', 'music', 29.99, 50);
+            
+    
---
+++ bamazonCustomer.js
@@ -0,0 +1,117 @@
+
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+var Table = require('cli-table2');
+
+//sql connection
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+	user: 'root',
+	password: 'Viviana01',
+	database: 'bamazon_db', 
+});
+
+
+//Provides connection status
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('connected as id' + connection.threadId + '\n\n');
+    start();
+});
+
+//Performs initial query of table of products
+    var start = function() {
+        connection.query('SELECT * FROM Products', function(err, res) {
+            console.log('---------------------------------');
+            console.log('Available Bamazon Products');
+            console.log('---------------------------------');
+
+            //New Table instance to format returned sql data
+            var table = new Table({
+                head: ['item_id', 'product_name', 'price', 'stock_quantity'],
+                colWidths: [10, 40, 10, 20]
+            });
+            for (var i=0; i < res.length; i++) {
+                var productArray = [res[i].item_id, res[i].product_name, res[i].price, res[i].stock_quantity];
+                table.push(productArray);
+            }
+            console.log(table.toString());
+            buyItem();
+        });
+    };
+//Prompts the customer on which item to buy
+    var buyItem = function() {
+        inquirer.prompt([{
+            name: "Item",
+            type: "input",
+            message: "Choose the ID of the Item you would like to buy",
+            validate: function(value) {
+
+                //Validates answer
+                if (isNaN(value) === false) {
+                    return true;
+                } else {
+                    console.log("\nPlease enter only the item ID of the item you'd like to buy\n");
+                    return false;
+                }
+            }
+        },
+
+            //Prompts the customer for the quantity
+            {
+            name: "Qty",
+            type: "input",
+            message: "How many would you like to buy?",
+            validate: function(value) {
+                //validates answer
+                if (isNaN(value) === false) {
+                    return true;
+                } else {
+                    console.log("\nPlease enter a valid quantity\n");
+                    return false;
+                }
+            }
+        }]).then(function(answer) {
+            var itemInv = parseInt(answer.Qty);
+
+            //Queries the database
+            connection.query("SELECT * FROM Products WHERE ?", [{item_id: answer.Item}], function(err, data) {
+                if (err) throw err;
+
+                //Checks if sufficient quantity exists
+                if (data[0].stock_quantity < itemInv) {
+                    console.log("We're sorry, that product is currently out of stock\n");
+                    console.log("Please choose another product\n");
+                    start();
+                } else {
+
+                    //If quantity exists updates database
+                    var updateQty = data[0].stock_quantity - itemInv;
+                    var totalPrice = data[0].price * itemInv;
+                    connection.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?', [updateQty, answer.Item], function(err, results) {
+                        if(err) {
+                            throw err;
+                        } else {
+                            console.log("Purchase complete!\n");
+                            console.log("Your total cost is: $ " + totalPrice);
+
+                            //Asks the buyer if they would like to continue
+                            inquirer.prompt({
+                                name: "buyMore",
+                                type: "confirm",
+                                message: "Would you like to buy another product?",
+                            }).then(function(answer) {
+                                if (answer.buyMore === true) {
+                                    start();
+                                } else {
+                                    console.log("Thank your for shopping with Bamazon!");
+                                    connection.end();
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        });
+    };
