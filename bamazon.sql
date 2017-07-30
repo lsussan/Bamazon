e1897e80b01ab7a38a7cd1d2df783c5bd303c1b5
louis.sussan@gmail.com <louis.sussan@gmail.com>
2017-07-30 14:10:11 -0700

    update mysql

--- bamazon.sql
+++ bamazon.sql
@@ -21,6 +21,16 @@ INSERT INTO products (product_name, department_name, price, stock_quantity)
 			('Air Force Ones', 'shoes', 129.99, 25),
 			('iphone charger', 'electronics', 9.99, 100),
 			('Magic Johnson Autographed Basketball', 'memorabilia', 999.99, 2),
-			('Arcade Fire Funeral LP', 'music', 29.99, 50);
+			('Arcade Fire Funeral LP', 'music', 29.99, 50),
+            ('Liverpool Soccer Kit', 'sporting goods', 119.95, 25),
+            ('Dodgers Jersey', 'sporting goods', 125.99, 20),
+            ('Apple Iphone 6s', 'electronics', 399.95, 50),
+            ('10 pack batteries AA', 'electronics', 5.99, 55),
+            ('Playstation 4 Console', 'electronics', 499.99, 10);
             
+ select * from products;
  
+ 
+#DELETE from products 
+#WHERE item_id = 10;		
+select * from products;    
